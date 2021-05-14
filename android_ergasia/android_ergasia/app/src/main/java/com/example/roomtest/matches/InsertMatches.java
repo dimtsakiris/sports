2c478c783d12692c01528971a908c869109d30ca
stratoseus1998 <tsalikiseus@gmail.com>
2021-05-14 12:01:10 +0300

    14/5

--- android_ergasia/android_ergasia/app/src/main/java/com/example/roomtest/matches/InsertMatches.java
+++ android_ergasia/android_ergasia/app/src/main/java/com/example/roomtest/matches/InsertMatches.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.roomtest.R;
+import com.example.roomtest.team.Team;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.fragment.app.Fragment;
@@ -15,6 +18,7 @@ import androidx.fragment.app.Fragment;
 public class InsertMatches extends Fragment {
 EditText csport,tname1,cteam1,score1,tname2,cteam2,score2,date,city,country;
 Button btn;
+
 public static FirebaseFirestore db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,8 +39,56 @@ public static FirebaseFirestore db;
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                int Var_teamId= 0;
+                try {
+                    Var_teamId = Integer.parseInt(cteam1.getText().toString());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Could not parse " + ex);
+                }
+                int codesport=Integer.parseInt(csport.getText().toString());
+                String name1=tname1.getText().toString();
+                int s1=Integer.parseInt(score1.getText().toString());
+                String name2=tname2.getText().toString();
+                int tid2=Integer.parseInt(cteam2.getText().toString());
+                int s2=Integer.parseInt(score2.getText().toString());
+                int Date=Integer.parseInt(date.getText().toString());
+                String ci=city.getText().toString();
+                String Country=country.getText().toString();
 
 
+
+                try{
+                   Team team=new Team();
+                   team.setId(codesport);
+                   team.setName(name1);
+                   team.setName(name2);
+                   team.setScore1(s1);
+                   team.setId(tid2);
+                   team.setScore2(s2);
+                   team.setEstablishment(Date);
+                   team.setStadium(ci);
+                   team.setCountry(Country);
+
+                    db.document(""+Var_teamId).set(team).addOnCompleteListener((task)-> {
+                        Toast.makeText(getActivity(),"Added Record",Toast.LENGTH_LONG).show();})
+                            .addOnFailureListener((e)->{Toast.makeText(getActivity(),"Fail",Toast.LENGTH_LONG).show();});
+
+
+                }catch (Exception e){
+                    String message = e.getMessage();
+                    Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+                }
+
+                csport.setText("");
+                tname1.setText("");
+                cteam1.setText("");
+                score1.setText("");
+                tname2.setText("");
+                cteam2.setText("");
+                score2.setText("");
+                date.setText("");
+                city.setText("");
+                country.setText("");
 
 
             }
--- android_ergasia/android_ergasia/app/src/main/java/com/example/roomtest/team/Team.java
+++ android_ergasia/android_ergasia/app/src/main/java/com/example/roomtest/team/Team.java
@@ -41,6 +41,24 @@ public class Team {
         this.country = country;
         this.establishment = establishment;
     }*/
+ private  int score1;
+ private int score2;
+
+    public void setScore1(int score1) {
+        this.score1 = score1;
+    }
+
+    public void setScore2(int score2) {
+        this.score2 = score2;
+    }
+
+    public int getScore1() {
+        return score1;
+    }
+
+    public int getScore2() {
+        return score2;
+    }
 
     public int getId() {
         return id;
